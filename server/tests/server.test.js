9365200aad132fed9f6bdcf7c21641fcaa33c5d2
Hemanth Varra <hemanth.varra@tecnotree.com>
2017-11-20 18:59:11 +0530

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
   const expect = require('expect');
   const request = require('supertest');
+  const {ObjectID} = require('mongodb');
 
   const {app} = require('./../server');
   const {Todo} = require('./../models/todo');
 
   const todos = [{
+    _id: new ObjectID(),
     text: 'First test todo'
   }, {
+    _id: new ObjectID(),
     text: 'Second test todo'
   }];
 
@@ -39,7 +42,6 @@
           }).catch((e) => done(e));
         });
     });
-  });
 
   it('should not create todo with invalid body data', (done) => {
       request(app)
@@ -57,7 +59,10 @@
             done();
           }).catch((e) => done(e));
         });
+    });
   });
+
+
 
   describe('GET /todos', () => {
     it('should get all todos', (done) => {
@@ -69,4 +74,39 @@
        })
        .end(done);
     });
+  });
+
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+      // console.log(+todos[0]._id.toHexString());
+      var hexId = todos[0]._id.toHexString();
+      request(app)
+        // .get('/todos/${todos[0]._id.toHexString()}')
+        // .get('/todos/' +'${todos[0]._id.toHexString()}')
+        .get('/todos/'+hexId)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end(done);
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+      request(app)
+        // .get('/todos/${todos[0]._id.toHexString()}')
+        .get('/todos/123abc')
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 if todo not found', (done) => {
+      var hexId = new ObjectID().toHexString();
+      request(app)
+        // .get('/todos/${todos[0]._id.toHexString()}')
+        .get('/todos/'+hexId)
+        .expect(404)
+        .end(done);
+    });
+
   });
