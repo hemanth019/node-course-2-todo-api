9c0821909a859dd07b8b4ba1cfb1ea1f76b6f1d6
Hemanth Varra <hemanth.varra@tecnotree.com>
2017-11-26 11:56:18 +0530

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@
     text: 'First test todo'
   }, {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
   }];
 
   beforeEach((done) => {
@@ -140,5 +142,54 @@ describe('GET /todos/:id', () => {
             .delete('/todos/123abc')
             .expect(404)
             .end(done);
+    });
+  });
+
+  describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+      //grab id of first item
+      var hexId = todos[0]._id.toHexString();
+      var text = "Its my life";
+
+      request(app)
+        .patch(`/todos/${hexId}`)
+        .send({
+          completed: true,
+          text
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todo.completed).toBe(true);
+          expect(res.body.todo.completedAt).toBeA('number');
+          expect(res.body.todo.text).toBe(text);
+          })
+        .end(done);
+      //update text, set completed true
+      //200
+      //text is changed, completed is true, completedAt is a number .toBeA
+
+    });
+
+    it('should clear completedAt when todo is not completed', (done) => {
+      //grab id of second todo item
+      var hexId = todos[1]._id.toHexString();
+      var text = "Its my 2nd life";
+      //update text, set completed to false
+      request(app)
+        .patch(`/todos/${hexId}`)
+        .send({
+          completed: false,
+          text
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todo.completed).toBe(false);
+          expect(res.body.todo.completedAt).toNotExist();
+          expect(res.body.todo.text).toBe(text);
+          })
+        .end(done);
+      //200
+      //text is changed, completed false, completedAt is null, toNotExist
+
     });
   });
