5d4cf5125029beafb6ad9519452cee542e3384ee
Hemanth Varra <hemanth.varra@tecnotree.com>
2017-12-10 14:19:43 +0530

    spell mistake

--- server/models/user.js
+++ server/models/user.js
@@ -82,7 +82,7 @@ var UserSchema = new mongoose.Schema({
         return Promise.reject();
       }
 
-      return new Prmise((resolve, reject) => {
+      return new Promise((resolve, reject) => {
           //Use bcrypt.compare to compare password and user.password
           bcrypt.compare(password, user.password, (err, res) => {
             if(res) {
